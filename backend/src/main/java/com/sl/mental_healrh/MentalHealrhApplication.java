acf136c589a984e5922dece3c5f9dae2b0819d81
liu <2490105648@qq.com>
2026-03-06 19:37:13 +0800

    init commit

---
+++ backend/src/main/java/com/sl/mental_healrh/MentalHealrhApplication.java
@@ -0,0 +1,13 @@
+package com.sl.mental_healrh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalHealrhApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MentalHealrhApplication.class, args);
+  }
+
+}
---
+++ backend/src/test/java/com/sl/mental_healrh/MentalHealrhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sl.mental_healrh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalHealrhApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

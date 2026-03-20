42ff9a150a63147f2601a4c8a5dc902045958fea
liu <2490105648@qq.com>
2026-03-20 20:47:48 +0800

    给辅导员安排班级

--- backend/src/main/java/com/sl/mentalhealth/repository/CounselorRepository.java
+++ backend/src/main/java/com/sl/mentalhealth/repository/CounselorRepository.java
@@ -1,10 +1,7 @@
 package com.sl.mentalhealth.repository;
 
 import com.sl.mentalhealth.entity.Counselor;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CounselorRepository extends JpaRepository<Counselor, String> {
-
-  Optional<Counselor> findFirstByCollegeAndGrade(String college, String grade);
 }
